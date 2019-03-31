c0f9694c4153bd1de9d1e8e5ffef8047c1ed921a
Matteo Montanari <matteo@italinux.com>
2019-03-31 12:56:18 +0200

    Add files via upload

---
+++ themes/lazy5basic/page_theme.php
@@ -0,0 +1,113 @@
+<?php
+/**
+.---------------------------------------------------------------------.
+|  @package: Theme Lazy5basic (a.k.a. theme Personal Pro)
+|  @version: v1.2.4 (31 March 2019)
+|  @link:    http://italinux.com/personal-pro
+|  @docs:    http://italinux.com/theme-personal-pro
+|
+|  @author: Matteo Montanari <matteo@italinux.com>
+|  @link:   http://matteo-montanari.com
+'---------------------------------------------------------------------'
+.---------------------------------------------------------------------------.
+| @copyright (c) 2019                                                       |
+| ------------------------------------------------------------------------- |
+| @license: Concrete5.org Marketplace Commercial Add-Ons & Themes License   |
+|           http://concrete5.org/help/legal/commercial_add-on_license       |
+|           or just: file://theme_lazy5basic/LICENSE.TXT                    |
+|                                                                           |
+| This program is distributed in the hope that it will be useful - WITHOUT  |
+| ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or     |
+| FITNESS FOR A PARTICULAR PURPOSE.                                         |
+'---------------------------------------------------------------------------'
+*/
+namespace Concrete\Package\ThemeLazy5basic\Theme\Lazy5basic;
+
+use Concrete\Package\ThemeLazy5basic\Controller as cPackage;
+use Concrete\Core\Page\Theme\Theme;
+
+class PageTheme extends Theme
+{
+    
+    protected $pThemeGridFrameworkHandle = 'bootstrap3';
+
+
+    public function getThemeDefaultBlockTemplates()
+    {
+
+        return array(
+            'l5b_menu' => 'sticky',
+            'l5b_banner' => 'clean_style',
+            'l5b_what_i_do' => 'items_style_clean',
+            'l5b_about_me' => 'image_clean_left',
+            'l5b_contacts' => 'flatty_yellow_light',
+            'l5b_social_media' => '',
+            'l5b_footer' => 'show_credits',
+        );
+    }
+
+    public function registerAssets() 
+    {
+
+        /** * * * * * * * * * * * * * * * * * * * * * * * * * *
+        * Avoid Conflicts
+        * JS + CSS to Avoid
+        */
+        // do NOT load: Bootstrap
+        $this->providesAsset('css', 'bootstrap/*');
+
+        // do NOT load: LightBox
+        $this->providesAsset('css', 'core/frontend/*');
+
+        // do NOT load: Font-Awesome
+        // $this->providesAsset('css', 'font-awesome');
+
+        /** * * * * * * * * * * * * * * * * * * * * * * * * * *
+        * Required
+        * JS + CSS Required
+        */ 
+        // JS JQuery v1.11.3
+        $this->requireAsset('javascript', 'jquery');
+
+        // Import Theme Assets CSS & JS
+        $this->requireAsset('jst.theme.assets');
+
+        // Import Animations Assets CSS & JS
+        $this->requireAsset('jst.animate.assets');
+    }
+
+    public function getThemeDisplayName($format = 'html')
+    {
+        return cPackage::create()->getPackageName($format);
+    }
+
+    public function getThemeDescription()
+    {
+       return t('A perfectly crafted theme that uses all the power of %s', 'concrete5');
+    }
+
+    public function getThemePrefix()
+    {
+        return cPackage::create()->getPackagePrefix();
+    }
+
+    public function getVersion() 
+    {
+        return 'v' . cPackage::create()->getPackageVersion();
+    }
+
+    public function getAreasNames()
+    {
+        return cPackage::create()->getAreasNames();
+    }
+
+    public function getThemeResponsiveImageMap() 
+    {
+
+        return array(
+            'large' => '900px',
+            'medium' => '768px',
+            'small' => '0'
+        );
+    }
+}
