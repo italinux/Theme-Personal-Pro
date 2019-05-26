f476f76eb9fc4257f982881e9d82d076d8ef5052
Matteo Montanari <matteo@italinux.com>
2019-05-26 12:45:45 +0200

    New Features

--- blocks/l5b_social_media/controller.php
+++ blocks/l5b_social_media/controller.php
@@ -1,4 +1,26 @@
 <?php
+/**
+.---------------------------------------------------------------------.
+|  @package: Theme Lazy5basic (a.k.a. theme Personal Pro)
+|  @version: Latest on Github
+|  @link:    http://italinux.com/personal-pro
+|  @docs:    http://italinux.com/theme-personal-pro
+|
+|  @author: Matteo Montanari <matteo@italinux.com>
+|  @link:   http://matteo-montanari.com
+'---------------------------------------------------------------------'
+.---------------------------------------------------------------------------.
+| @copyright (c) 2019                                                       |
+| ------------------------------------------------------------------------- |
+| @license: Concrete5.org Marketplace Commercial Add-Ons & Themes License   |
+|           http://concrete5.org/help/legal/commercial_add-on_license       |
+|           or just: file://theme_lazy5basic/LICENSE.TXT                    |
+|                                                                           |
+| This program is distributed in the hope that it will be useful - WITHOUT  |
+| ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or     |
+| FITNESS FOR A PARTICULAR PURPOSE.                                         |
+'---------------------------------------------------------------------------'
+*/
 namespace Concrete\Package\ThemeLazy5basic\Block\L5bSocialMedia;
 
 use Concrete\Package\ThemeLazy5basic\Src\Utils\Utils as BlockUtils;
