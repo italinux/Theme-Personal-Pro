ac219b941cd38b527eb6a0442504a0e32ed0ba65
Matteo Montanari <matteo@italinux.com>
2023-01-27 13:12:47 +0700

    New Features

--- controller.php
+++ controller.php
@@ -39,7 +39,7 @@ class Controller extends Package
 {
 
     protected $pkgHandle = 'theme_lazy5basic';
-    protected $pkgVersion = '2.5.4';
+    protected $pkgVersion = '2.5.5';
     protected $pkgPrefix = 'l5b';
     protected $pkgAlias = 'Personal Pro';
 
